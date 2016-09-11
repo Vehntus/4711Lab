09b8c619fc663d1cfac2d41d2c5b52a0e164bbd1
Vehntus <Vehntus@github.com>
2016-09-10 23:19:29 -0700

    Added comments

--- Student.php
+++ Student.php
@@ -7,12 +7,14 @@
  */
 
 /**
- * Description of Student
- *
+ *  The Student Class is used to for the creation of student objects containing
+ *  the students name, emails, and grades.
+ * 
  * @author Vehntus
  */
 class Student {
     
+    // The Student constructor
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
@@ -20,24 +22,32 @@ class Student {
         $this->grades = array();
     }
     
+    // The add_email function is used to add a students email(s)
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
     
+    // The add_grade function is used to add a students grade(s)
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    // The average function is used to calculate the students grades average
     function average() {
         $total = 0;
+        // Loops to sum multiple grades
         foreach ($this->grades as $value)
             $total += $value;
         return $total / count($this->grades);
     }
     
+    /* The toString function is used to return the students information
+     * as a string
+     */
     function toString() {
         $result = $this->first_name . ' ' . $this->surname;
         $result .= ' (' . $this->average() . ")\n";
+        // Loops to return multiple emails
         foreach ($this->emails as $which => $what)
             $result .= $which . ': ' . $what . "\n";
         $result .= "\n";
